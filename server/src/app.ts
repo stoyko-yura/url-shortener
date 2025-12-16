55386c5e6557ea3b2593b202dfc9a0782c3b9957
stoyko-yura <stoyko.yuri@gmail.com>
2025-12-16 10:27:59 +0200

    chore(server): add CORS middleware

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,11 @@
+import cors from "cors";
 import express from "express";
 import urlRoutes from "./routes/url.routes";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/url", urlRoutes);
 
